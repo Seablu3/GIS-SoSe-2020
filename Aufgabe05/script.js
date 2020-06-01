054e8b697734a7c2b866aa1f75036572bbb0af7f
Seablu3 <64130114+Seablu3@users.noreply.github.com>
2020-06-01 15:56:07 +0200

    Update script.js

--- Aufgabe05/script.js
+++ Aufgabe05/script.js
@@ -1,73 +1,75 @@
 "use strict";
 //Bestseller wolken
-let cirrus = { img: "pics/cirrus.jpg", name: "Cirruswolke", description: "Nice Wolke", price: "11,50€" };
-let cumulonimbus = { img: "pics/cumulonimbus.jpg", name: "Cumulonimbuswolke", description: "Wenn du Gewitter magst, genau deine Wolke", price: "1100,00€" };
-let stratus = { img: "pics/stratus.jpg", name: "Stratuswolke", description: "Ein 'Furtwangen Depression' Classic", price: "2,00€" };
-let nimbostratus = { img: "pics/nimbostratus.jpg", name: "Nimbostratuswolke€", description: "Erzeugen lange anhaltenden Regen: Deutsche Landwirte küssen deine Augen.", price: "200,00€" };
-let altostratus = { img: "pics/altostratus.jpg", name: "Altostratuswolke", description: "Absoluter Schmodder", price: "10,00€" };
-let cirrostratus = { img: "pics/cirrostratus.jpg", name: "Cirrostratuswolke", description: "Fusion aus Cirrus und Stratus: Erzeugt (mit Glück) schöne Kreise", price: "50,00€" };
+let cirrus = { img: "pics/cirrus.jpg", name: "Cirruswolke", description: "Nice Wolke", price: 11.50 + "€" };
+let cumulonimbus = { img: "pics/cumulonimbus.jpg", name: "Cumulonimbuswolke", description: "Wenn du Gewitter magst, genau deine Wolke", price: 1100.00 + "€" };
+let stratus = { img: "pics/stratus.jpg", name: "Stratuswolke", description: "Ein 'Furtwangen Depression' Classic", price: 2.00 + "€" };
+let nimbostratus = { img: "pics/nimbostratus.jpg", name: "Nimbostratuswolke€", description: "Erzeugen lange anhaltenden Regen: Deutsche Landwirte küssen deine Augen.", price: 200.00 + "€" };
+let altostratus = { img: "pics/altostratus.jpg", name: "Altostratuswolke", description: "Absoluter Schmodder", price: 10.00 + "€" };
+let cirrostratus = { img: "pics/cirrostratus.jpg", name: "Cirrostratuswolke", description: "Fusion aus Cirrus und Stratus: Erzeugt (mit Glück) schöne Kreise", price: 50.00 + "€" };
 //Array Bestseller
-let BestsellerW = [cirrus, cumulonimbus, stratus, nimbostratus, altostratus, cirrostratus];
+let bestsellerW = [cirrus, cumulonimbus, stratus, nimbostratus, altostratus, cirrostratus];
 //Unkonventionelle Wolken
-let kraftwerkabdampf = { img: "pics/AKW.jpg", name: "Kraftwerkabdampf", description: "pfui", price: "nicht verfügbar" };
-let lilawolken = { img: "pics/lilawolken.jpg", name: "Lila Wolken", description: "30 Grad, du kühlst dein Kompf am", price: "Fensterglas" };
-let aschewolke = { img: "pics/vulkanausbruch.jpg", name: "Aschewolke", description: "Abriss", price: "1,50€" };
+let kraftwerkabdampf = { img: "pics/AKW.jpg", name: "Kraftwerkabdampf", description: "pfui", price: 777777777777.77 + "€" };
+let lilawolken = { img: "pics/lilawolken.jpg", name: "Lila Wolken", description: "30 Grad, du kühlst dein Kompf am", price: 0.01 + "€" };
+let aschewolke = { img: "pics/vulkanausbruch.jpg", name: "Aschewolke", description: "Abriss", price: 1.50 + "€" };
 //Array Unkonventionelle
-let UnkonventionelleW = [kraftwerkabdampf, lilawolken, aschewolke];
+let unkonventionelleW = [kraftwerkabdampf, lilawolken, aschewolke];
 //Special Wolken
-let wolke7 = { img: "pics/wolke7.jpg", name: "Wolke7", description: "Die wahre Liebe", price: "696969,69€" };
-let andereWolke = { img: "pics/daweeeeeeeed.jpg", name: "Diese andere Wolke", description: "gas gas", price: "4,20€" };
-let icloud = { img: "pics/icloud.jpg", name: "iCloud Speicher", description: "scameriono", price: "1111,10€" };
+let wolke7 = { img: "pics/wolke7.jpg", name: "Wolke7", description: "Die wahre Liebe", price: 696969.69 + "€" };
+let andereWolke = { img: "pics/daweeeeeeeed.jpg", name: "Diese andere Wolke", description: "gas gas", price: 4.20 + "€" };
+let icloud = { img: "pics/icloud.jpg", name: "iCloud Speicher", description: "scameriono", price: 1111.10 + "€" };
 //Array Specials
-let SpecialsW = [wolke7, andereWolke, icloud];
+let specialsW = [wolke7, andereWolke, icloud];
 //Bestseller for schleife 
-for (let index = 0; index < BestsellerW.length; index++) {
+for (let index = 0; index < bestsellerW.length; index++) {
     //Div erstellen 
     let newDiv = document.createElement("div");
     newDiv.id = "div1" + index;
     document.getElementById("grid1")?.appendChild(newDiv);
     //Bild hinzuziehen
     let imgElement = document.createElement("img");
-    imgElement.src = BestsellerW[index].img;
+    imgElement.src = bestsellerW[index].img;
     document.getElementById("div1" + index)?.appendChild(imgElement);
     //Name hinzuziehen
     let name = document.createElement("h2");
-    name.innerHTML = BestsellerW[index].name;
+    name.innerHTML = bestsellerW[index].name;
     document.getElementById("div1" + index)?.appendChild(name);
     //Beschreibung
     let description = document.createElement("p");
-    description.innerHTML = BestsellerW[index].description;
+    description.innerHTML = bestsellerW[index].description;
     document.getElementById("div1" + index)?.appendChild(description);
     //Preis
     let price = document.createElement("p");
-    price.innerHTML = BestsellerW[index].price;
+    price.innerHTML = bestsellerW[index].price;
     document.getElementById("div1" + index)?.appendChild(price);
+    // let price: HTMLElement = document.createElement("p");
+    // let produktPreisStrong: HTMLElement = document.createElement("strong");
+    // artikelcontainer.appendChild(produktPreis);
     //Kaufen
     let kaufen = document.createElement("button");
     kaufen.innerHTML = "Kaufen";
     document.getElementById("div1" + index)?.appendChild(kaufen);
 }
 //Unkonventionelle for schleife 
-for (let index = 0; index < UnkonventionelleW.length; index++) {
+for (let index = 0; index < unkonventionelleW.length; index++) {
     //Div erstellen
     let newDiv = document.createElement("div");
     newDiv.id = "div2" + index;
     document.getElementById("grid2")?.appendChild(newDiv);
     //Bild hinzuziehen
     let imgElement = document.createElement("img");
-    imgElement.src = UnkonventionelleW[index].img;
+    imgElement.src = unkonventionelleW[index].img;
     document.getElementById("div2" + index)?.appendChild(imgElement);
     //Name hinzuziehen
     let name = document.createElement("h2");
-    name.innerHTML = UnkonventionelleW[index].name;
+    name.innerHTML = unkonventionelleW[index].name;
     document.getElementById("div2" + index)?.appendChild(name);
     //Beschreibung
     let description = document.createElement("p");
-    description.innerHTML = UnkonventionelleW[index].description;
+    description.innerHTML = unkonventionelleW[index].description;
     document.getElementById("div2" + index)?.appendChild(description);
     //Preis
     let price = document.createElement("p");
-    price.innerHTML = UnkonventionelleW[index].price;
     document.getElementById("div2" + index)?.appendChild(price);
     //Kaufen
     let kaufen = document.createElement("button");
@@ -75,26 +77,26 @@ for (let index = 0; index < UnkonventionelleW.length; index++) {
     document.getElementById("div2" + index)?.appendChild(kaufen);
 }
 //Specials for schleife 
-for (let index = 0; index < SpecialsW.length; index++) {
+for (let index = 0; index < specialsW.length; index++) {
     //Div erstellen
     let newDiv = document.createElement("div");
     newDiv.id = "div3" + index;
     document.getElementById("grid3")?.appendChild(newDiv);
     //Bild hinzuziehen
     let imgElement = document.createElement("img");
-    imgElement.src = SpecialsW[index].img;
+    imgElement.src = specialsW[index].img;
     document.getElementById("div3" + index)?.appendChild(imgElement);
     //Name hinzuziehen
     let name = document.createElement("h2");
-    name.innerHTML = SpecialsW[index].name;
+    name.innerHTML = specialsW[index].name;
     document.getElementById("div3" + index)?.appendChild(name);
     //Beschreibung
     let description = document.createElement("p");
-    description.innerHTML = SpecialsW[index].description;
+    description.innerHTML = specialsW[index].description;
     document.getElementById("div3" + index)?.appendChild(description);
     //Preis
     let price = document.createElement("p");
-    price.innerHTML = SpecialsW[index].price;
+    price.innerHTML = specialsW[index].price;
     document.getElementById("div3" + index)?.appendChild(price);
     //Kaufen
     let kaufen = document.createElement("button");
