21a41b601e35f739725a93a1d3cada8cfb27b273
Seablu3 <64130114+Seablu3@users.noreply.github.com>
2020-06-03 10:39:29 +0200

    Update script.ts

--- Aufgabe05/script.ts
+++ Aufgabe05/script.ts
@@ -1,26 +1,33 @@
-"use strict";
+
 
 //Bestseller wolken
-let cirrus = { img: "pics/cirrus.jpg", name: "Cirruswolke", description: "Nice Wolke", price: 11.50 + "€" };
-let cumulonimbus = { img: "pics/cumulonimbus.jpg", name: "Cumulonimbuswolke", description: "Wenn du Gewitter magst, genau deine Wolke", price: 1100.00 + "€" };
-let stratus = { img: "pics/stratus.jpg", name: "Stratuswolke", description: "Ein 'Furtwangen Depression' Classic", price: 2.00 + "€"};
-let nimbostratus = { img: "pics/nimbostratus.jpg", name: "Nimbostratuswolke€", description: "Erzeugen lange anhaltenden Regen: Deutsche Landwirte küssen deine Augen.", price: 200.00 + "€" };
-let altostratus = { img: "pics/altostratus.jpg", name: "Altostratuswolke", description: "Absoluter Schmodder", price: 10.00 + "€" };
-let cirrostratus = { img: "pics/cirrostratus.jpg", name: "Cirrostratuswolke", description: "Fusion aus Cirrus und Stratus: Erzeugt (mit Glück) schöne Kreise", price: 50.00 + "€" };
+interface Artikel {
+    img: string;
+    name: string;
+    description: string;
+    price: number;
+}
+
+let cirrus: Artikel = { img: "pics/cirrus.jpg", name: "Cirruswolke", description: "Nice Wolke", price: 11.50  };
+let cumulonimbus: Artikel = { img: "pics/cumulonimbus.jpg", name: "Cumulonimbuswolke", description: "Wenn du Gewitter magst, genau deine Wolke", price: 1100.00  };
+let stratus: Artikel = { img: "pics/stratus.jpg", name: "Stratuswolke", description: "Ein 'Furtwangen Depression' Classic", price: 2.00 };
+let nimbostratus: Artikel = { img: "pics/nimbostratus.jpg", name: "Nimbostratuswolke€", description: "Erzeugen lange anhaltenden Regen: Deutsche Landwirte küssen deine Augen.", price: 200.00  };
+let altostratus: Artikel = { img: "pics/altostratus.jpg", name: "Altostratuswolke", description: "Absoluter Schmodder", price: 10.00 };
+let cirrostratus: Artikel = { img: "pics/cirrostratus.jpg", name: "Cirrostratuswolke", description: "Fusion aus Cirrus und Stratus: Erzeugt (mit Glück) schöne Kreise", price: 50.00 };
 //Array Bestseller
 let bestsellerW=[cirrus, cumulonimbus, stratus, nimbostratus, altostratus, cirrostratus];
 
 //Unkonventionelle Wolken
-let kraftwerkabdampf = { img: "pics/AKW.jpg", name: "Kraftwerkabdampf", description: "pfui", price: 777777777777.77 + "€" };
-let lilawolken = { img: "pics/lilawolken.jpg", name: "Lila Wolken", description: "30 Grad, du kühlst dein Kompf am Fensterglas", price: 0.01 + "€" };
-let aschewolke = { img: "pics/vulkanausbruch.jpg", name: "Aschewolke", description: "Abriss", price: 1.50 + "€" };
+let kraftwerkabdampf: Artikel = { img: "pics/AKW.jpg", name: "Kraftwerkabdampf", description: "pfui", price: 777777777777.77 };
+let lilawolken: Artikel = { img: "pics/lilawolken.jpg", name: "Lila Wolken", description: "30 Grad, du kühlst dein Kompf am Fensterglas", price: 0.01};
+let aschewolke: Artikel = { img: "pics/vulkanausbruch.jpg", name: "Aschewolke", description: "Abriss", price: 1.50};
 //Array Unkonventionelle
 let unkonventionelleW=[kraftwerkabdampf, lilawolken, aschewolke];
 
 //Special Wolken
-let wolke7 = { img: "pics/wolke7.jpg", name: "Wolke7", description: "Die wahre Liebe", price: 696969.69 + "€" };
-let andereWolke = { img: "pics/daweeeeeeeed.jpg", name: "Diese andere Wolke", description: "gas gas", price: 4.20 + "€" };
-let icloud = { img: "pics/icloud.jpg", name: "iCloud Speicher", description: "scameriono", price: 1111.10 + "€" };
+let wolke7: Artikel = { img: "pics/wolke7.jpg", name: "Wolke7", description: "Die wahre Liebe", price: 696969.69};
+let andereWolke: Artikel = { img: "pics/daweeeeeeeed.jpg", name: "Diese andere Wolke", description: "gas gas", price: 4.20};
+let icloud: Artikel = { img: "pics/icloud.jpg", name: "iCloud Speicher", description: "scameriono", price: 1111.10};
 //Array Specials
 let specialsW = [wolke7, andereWolke, icloud];
 
@@ -46,13 +53,10 @@ for (let index = 0; index < bestsellerW.length; index++) {
     document.getElementById("div1" + index)?.appendChild(description);
     //Preis
     let price = document.createElement("p");
-    price.innerHTML = bestsellerW[index].price;
+    price.innerHTML = bestsellerW[index].price + "€";
     document.getElementById("div1" + index)?.appendChild(price) ;
 
-    // let price: HTMLElement = document.createElement("p");
-    // let produktPreisStrong: HTMLElement = document.createElement("strong");
-    // artikelcontainer.appendChild(produktPreis);
-
+    
     
     
     //Kaufen
@@ -81,7 +85,7 @@ for (let index = 0; index < unkonventionelleW.length; index++) {
     document.getElementById("div2" + index)?.appendChild(description);
     //Preis
     let price = document.createElement("p");
-    price.innerHTML = unkonventionelleW[index].price;
+    price.innerHTML = unkonventionelleW[index].price + " €";
     document.getElementById("div2" + index)?.appendChild(price);
     //Kaufen
     let kaufen = document.createElement("button");
@@ -109,11 +113,11 @@ for (let index = 0; index < specialsW.length; index++) {
     document.getElementById("div3" + index)?.appendChild(description);
     //Preis
     let price = document.createElement("p");
-    price.innerHTML = specialsW[index].price;
+    price.innerHTML = specialsW[index].price + "€";
     document.getElementById("div3" + index)?.appendChild(price);
     //Kaufen
     let kaufen = document.createElement("button");
     kaufen.innerHTML = "Kaufen";
     document.getElementById("div3" + index)?.appendChild(kaufen);
 }
-//# sourceMappingURL=script.js.map    
+//# sourceMappingURL=script.js.map    bobobo
