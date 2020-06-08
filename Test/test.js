077c26291c58755311d5f3bd6288722a76c7ad1d
Seablu3 <64130114+Seablu3@users.noreply.github.com>
2020-06-08 16:06:19 +0200

    Create test.js

---
+++ Test/test.js
@@ -0,0 +1,6 @@
+"use strict";
+document.querySelector("h2")?.addEventListener('click', boBobo);
+var myDiv = document.getElementById("myDiv");
+var Best = document.getElementById("Best");
+Best.addEventListener("click", function ({ myDiv, style, background = "lightblue" }) { });
+//# sourceMappingURL=test.js.map
