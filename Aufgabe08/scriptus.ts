0d41d25a1564f94ae2c6b847cd8182427f003b91
Seablu3 <64130114+Seablu3@users.noreply.github.com>
2020-06-19 13:21:10 +0200

    Create scriptus.ts

---
+++ Aufgabe08/scriptus.ts
@@ -0,0 +1,17 @@
+var Aufgabe08;
+(function (Aufgabe08) {
+    let initButton = document.getElementById("initButton");
+    initButton?.addEventListener("click", communicate);
+    async function communiacate() {
+        let formData = new FormData(document.forms[0]);
+        let url = "https://moselgisdorsch.herokuapp.com";
+        let query = new URLSearchParams(formData);
+        url = url + "?" + query.toString();
+        await fetch(url);
+        for (let entry of query) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
+    }
+})(Aufgabe08 || (Aufgabe08 = {}));
