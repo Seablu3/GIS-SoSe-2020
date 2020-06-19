df5271efe08e4fb2e8dcbef753e93294f3659321
Seablu3 <64130114+Seablu3@users.noreply.github.com>
2020-06-19 16:49:01 +0200

    Update scriptus.js

--- Aufgabe08/scriptus.js
+++ Aufgabe08/scriptus.js
@@ -2,7 +2,7 @@
 var Aufgabe08;
 (function (Aufgabe08) {
     let initButton = document.getElementById("initButton");
-    initButton?.addEventListener("click", communicate);
+    initButton.addEventListener("click", communicate);
     async function communicate() {
         let formData = new FormData(document.forms[0]);
         let url = "https://moselgisdorsch.herokuapp.com";
@@ -16,4 +16,20 @@ var Aufgabe08;
         }
     }
 })(Aufgabe08 || (Aufgabe08 = {}));
+// namespace Aufgabe08 {
+//     let submitBut: HTMLButtonElement = <HTMLButtonElement>document.getElementById("submitBut");
+//     submitBut.addEventListener("click", communicate);
+//     async function communicate(): Promise<void> {
+//         let formData: FormData = new FormData(document.forms[0]);
+//         let url: string = "https://timgissose2020.herokuapp.com";
+//         let query: URLSearchParams = new URLSearchParams(<any>formData);
+//         url = url + "?" + query.toString();
+//         await fetch(url);
+//         for (let entry of query) {
+//             console.log(entry);
+//             console.log("name: " + entry[0]);
+//             console.log("value: " + entry[1]);
+//         }
+//     }
+// }
 //# sourceMappingURL=scriptus.js.map
