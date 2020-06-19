3019355c38de148a3e6228e365063d2f603ffc37
Seablu3 <64130114+Seablu3@users.noreply.github.com>
2020-06-19 16:49:08 +0200

    Update scriptus.ts

--- Aufgabe08/scriptus.ts
+++ Aufgabe08/scriptus.ts
@@ -1,11 +1,11 @@
 var Aufgabe08;
 (function (Aufgabe08) {
-    let initButton = document.getElementById("initButton");
-    initButton?.addEventListener("click", communicate);
-    async function communicate() {
-        let formData = new FormData(document.forms[0]);
-        let url = "https://moselgisdorsch.herokuapp.com";
-        let query = new URLSearchParams(formData);
+    let initButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("initButton");
+    initButton.addEventListener("click", communicate);
+    async function communicate(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        let url: string = "https://moselgisdorsch.herokuapp.com";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
         url = url + "?" + query.toString();
         await fetch(url);
         for (let entry of query) {
@@ -15,3 +15,24 @@ var Aufgabe08;
         }
     }
 })(Aufgabe08 || (Aufgabe08 = {}));
+
+// namespace Aufgabe08 {
+    
+//     let submitBut: HTMLButtonElement = <HTMLButtonElement>document.getElementById("submitBut");
+//     submitBut.addEventListener("click", communicate);
+
+//     async function communicate(): Promise<void> {
+
+//         let formData: FormData = new FormData(document.forms[0]);
+//         let url: string = "https://timgissose2020.herokuapp.com";
+//         let query: URLSearchParams = new URLSearchParams(<any>formData);
+//         url = url + "?" + query.toString();
+//         await fetch(url);
+
+//         for (let entry of query) {
+//             console.log(entry);
+//             console.log("name: " + entry[0]);
+//             console.log("value: " + entry[1]);
+//         }
+//     }
+// }
