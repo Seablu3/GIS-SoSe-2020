80a8ba974c0364b0f075ea074ad02cfb1de45e26
Seablu3 <64130114+Seablu3@users.noreply.github.com>
2020-06-21 13:03:27 +0200

    Update scriptus.ts

--- Aufgabe08/scriptus.ts
+++ Aufgabe08/scriptus.ts
@@ -16,23 +16,3 @@ var Aufgabe08;
     }
 })(Aufgabe08 || (Aufgabe08 = {}));
 
-// namespace Aufgabe08 {
-    
-//     let submitBut: HTMLButtonElement = <HTMLButtonElement>document.getElementById("submitBut");
-//     submitBut.addEventListener("click", communicate);
-
-//     async function communicate(): Promise<void> {
-
-//         let formData: FormData = new FormData(document.forms[0]);
-//         let url: string = "https://timgissose2020.herokuapp.com";
-//         let query: URLSearchParams = new URLSearchParams(<any>formData);
-//         url = url + "?" + query.toString();
-//         await fetch(url);
-
-//         for (let entry of query) {
-//             console.log(entry);
-//             console.log("name: " + entry[0]);
-//             console.log("value: " + entry[1]);
-//         }
-//     }
-// }
